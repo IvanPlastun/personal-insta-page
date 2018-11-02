ee896b58e86eee4ddecf859b72a47371f2cccf23
Miarel <plastun.vanya@yandex.ru>
2018-11-02 20:37:24 +0300

    ДЗ2
    Добавлен блок с фотографиями. Написана фукнция для slide-show.

---
+++ docs/js/slideshow.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+
+    var sliderIndex = 0;
+
+    function showPhotos() {
+        var i;
+        var slides = $(".photo img");
+        //Цикл скрывающий все изображения
+        for(i = 0; i < slides.length; i++) {
+            $(slides[i]).hide();
+        }
+        //Увеличение счетчика, который отображает активное изображение
+        sliderIndex++;
+        //Сброс счетчика, если он превышает количество изображений выборки
+        if(sliderIndex > slides.length) 
+            sliderIndex = 1;
+        //Отображение активного изображения
+        $(slides[sliderIndex - 1]).show();
+        setTimeout(showPhotos, 500);
+    }
+    showPhotos();
+});
