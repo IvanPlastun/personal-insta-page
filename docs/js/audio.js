6eca497f6232ecd967722029418c38e47d46b396
Miarel <plastun.vanya@yandex.ru>
2018-11-04 20:38:10 +0300

    ДЗ4
    Внесены правки в файл стилей, добавлен audio-файл и разметка для него, элементы воспроизведения.  Добавлена функция, которая воспроизводит audio на странице.

--- docs/js/audio.js
+++ docs/js/audio.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 
+    var audio = $('#audioTrack')[0];
+    var $playBtn = $('#audio-play'),
+        $pauseBtn = $('#audio-pause'),
+        $audioControls = $('[data-type="audio-control"]');
+    //initAudioPlayer();
+    setTimeout(initAudioPlayer, 800);
+    function initAudioPlayer() {
+        $playBtn.parent().show();
+        audio.loop = true;
+        audio.play();
+        $audioControls.on('click', function() {
+            if(audio.paused) {
+                audio.play();
+                $playBtn.parent().hide();
+                $pauseBtn.parent().show();
+            } else {
+                audio.pause();
+                $pauseBtn.parent().hide();
+                $playBtn.parent().show();
+            }
+        });
+    }
+
+    //audio.play();
 
 
 });
